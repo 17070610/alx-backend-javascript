1c64a289253341c5582824bb2a38173cb128ae2d
17070610 <schimea@hotmail.com>
2025-04-17 17:37:06 +0200

    Catch me if you can

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    return promise
-      .then(() => ({status: 200, body: 'success'}))
-      .catch(() => new Error())
-      .finally(() => console.log('Got a response from API'));
-  });
+  return promise
+    .then(() => ({status: 200, body: 'success'}))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from API'));
 }
