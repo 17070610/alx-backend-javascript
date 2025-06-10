30277a3e60a6b305933b114a098329c1d01b8b8e
17070610 <schimea@hotmail.com>
2025-06-10 17:55:53 +0200

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,11 @@
 process.stdout.write('Welcome to ALX, what is your name?\n');
 
-process.stdin.on('data', (chunk) => {
-  const name = chunk.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
-  process.stdout.write('This important software is now closing\n');
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  console.log(`Your name is: ${name}`);
   process.exit();
+});
+
+process.on('exit', () => {
+  console.log('This important software is now closing');
 });
